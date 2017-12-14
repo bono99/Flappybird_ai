9adbce40b9803d7262aa0804d4d1ea7a001cd85f
wilmer <wilmer@wilmers-MacBook-Pro.local>
2017-12-15 00:38:25 +0100

    buffer class

---
+++ public/index.js
@@ -0,0 +1,15 @@
+import SpriteSheet from 'sprites.js';
+import {loadImage} from 'load_image.js';
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
+ctx.fillRect(0, 0, 70, 40);
+loadImage('res/sheet.png')
+.then(image => {
+	const sprites = new SpriteSheet(image, 16, 16);
+	sprites.define('ground', 0, 0);
+	sprites.draw('ground', ctx, 45, 45);
+	ctx.drawImage(image, 0, 0);
+});
